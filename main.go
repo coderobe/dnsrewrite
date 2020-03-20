e7dfdcfbb7c52cf3bb6bc1ac4e64997de06be9c3
Robin Broda <robin@broda.me>
2020-03-20 16:44:56 +0100

    Support redirecting to multiple upstreams; Add config file

--- main.go
+++ main.go
@@ -2,73 +2,68 @@ package main
 
 import (
 	"log"
-	"strings"
 	"os"
+	"io/ioutil"
 
 	"github.com/miekg/dns"
+	"gopkg.in/yaml.v2"
 )
 
 var client = new(dns.Client)
 
-func Query(data string) []dns.RR {
-	m := new(dns.Msg)
-	m.SetQuestion(dns.Fqdn(data), dns.TypeA)
+func handleDnsRequest(targetUpstream Upstream) func(dns.ResponseWriter, *dns.Msg) {
+	log.Printf("Generating handler for %s -> %s", targetUpstream.Target, targetUpstream.At)
+	return func(w dns.ResponseWriter, r *dns.Msg) {
+		m := r.Copy()
+		m.Compress = false
 
-	in, _, err := client.Exchange(m, os.Args[2])
-	if err == nil {
-		if len(in.Answer) > 0 {
-			return in.Answer
-		}
-	} else {
-		log.Printf("Query FAIL: %s", err.Error())
-	}
-	return nil
-}
-
-func parseQuery(m *dns.Msg, w dns.ResponseWriter) {
-	for _, q := range m.Question {
-		switch q.Qtype {
-		case dns.TypeA:
-			log.Printf("Query for A %s", q.Name)
-			if strings.HasSuffix(q.Name, dns.Fqdn(os.Args[3])) {
-				rr := Query(q.Name)
-				if len(rr) > 0 {
-					log.Printf("Valid, resolved to %s", rr)
-					m.Answer = rr
-				} else {
-					log.Printf("Did not resolve")
-					m.Rcode = dns.RcodeNameError
-				}
-			}
+		in, _, err := client.Exchange(m, targetUpstream.At)
+		m.SetReply(r)
+		if err != nil {
+			log.Printf("Query FAIL on (%s -> %s): %s", targetUpstream.Target, targetUpstream.At, err.Error())
+			m.Rcode = dns.RcodeServerFailure
+			w.WriteMsg(m)
+		} else {
+			w.WriteMsg(in)
 		}
 	}
 }
 
-func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Compress = false
-
-	switch r.Opcode {
-	case dns.OpcodeQuery:
-		parseQuery(m, w)
-	}
-
-	w.WriteMsg(m)
+type Upstream struct {
+	Target	string
+	At	string
+}
+type Conf struct {
+	Listen string
+	Upstreams []Upstream
 }
 
 func main() {
-	// attach request handler
-	dns.HandleFunc(".", handleDnsRequest)
+	log.Println("Starting dnsrewrite")
+
+	c := Conf{}
+	yamlFile, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Printf("yamlFile err %s", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		log.Printf("unmarshal: %v", err)
+	}
+
+	// attach request handlers
+	for _, t := range c.Upstreams {
+		dns.HandleFunc(t.Target, handleDnsRequest(t))
+	}
 
 	// start server
-	server := &dns.Server{Addr: os.Args[1], Net: "udp"}
-	log.Printf("Responder at %s", os.Args[1])
-	log.Printf("Upstream at %s", os.Args[2])
-	log.Printf("Filter for %s", os.Args[3])
-	err := server.ListenAndServe()
-	defer server.Shutdown()
+	server := &dns.Server{Addr: c.Listen, Net: "udp"}
+	log.Printf("Responder at %s", c.Listen)
+	log.Printf("Proxying for %v upstreams", len(c.Upstreams))
+
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Failed to start server: %s ", err.Error())
 	}
+	defer server.Shutdown()
 }
